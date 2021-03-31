ab1f4151fd6cb7d6a7c4a947b8762a76520e8a6c
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.com>
2021-03-31 00:17:42 -0400

    added inclusion tag to display user table

--- django_tutorial/first_app/templatetags/custom_tags.py
+++ django_tutorial/first_app/templatetags/custom_tags.py
@@ -1,8 +1,12 @@
 from django import template
 from datetime import datetime
+from django.contrib.auth import get_user_model
+from django.template.loader import get_template
+
+
+User = get_user_model()
+
 register = template.Library()
-
-
 # any number of custom tags, python functions and resister with regitry
 
 def modify_name(value, arg):
@@ -20,11 +24,30 @@ def modify_name(value, arg):
 
     return value
 
+register.filter("modify_name", modify_name)
 
 @register.simple_tag
 def current_time(formate_str):
     return datetime.now().strftime(formate_str)   # '12/2/2021
 
+class user:
+    pass
 
-register.filter("modify_name", modify_name)
+#@register.inclusion_tag('first_app/users.html')
+def show_users_table():
+    users = User.objects.all()
+    # import pdb;pdb.set_trace()
+    # obj = user()
+    # obj.first_name = "Badra"
+    # obj.last_name = "A"
+    # obj.username = "Abc"
+    # obj.email = "abc@gmail.com"
+    return {'users': users}
+
+# import pdb;pdb.set_trace()
+users_template = get_template('first_app/users.html')
+register.inclusion_tag(users_template)(show_users_table)
+
+
+
 
